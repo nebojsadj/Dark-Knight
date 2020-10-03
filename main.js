2079a22dfc0016fcc092cb12e858ec7f39a670a7
nebojsadj <djordjevicn87@gmail.com>
2020-10-03 09:51:59 +0200

    selectors

--- main.js
+++ main.js
@@ -1,36 +1,4 @@
 
-let counter = 1;
-let counter1 = 0;
-let counter2 = 0;
-let br = 0;
-const startWrapp = document.querySelector('#startWrapp');
-const wrapp = document.querySelector('#wrapp');
-const wrapp2 = document.querySelector('#wrapp2');
-let theme = document.querySelector('#theme');
-let audioJoker = document.querySelector('#audioJoker');
-let audioBatman = document.querySelector('#audioBatman');
-let sound = document.querySelector('#sound');
-let fightAudio = document.querySelector('#fight');
-let gameOwerAudio = document.querySelector('#gameOwer');
-
-let loop1;
-let loop2;
-let joker;
-let batman;
-let position;
-let positionLeft;
-let positionRight;
-let positionTop;
-let positionBottom;
-let greenBox;
-let jokerWeapon = document.querySelector('#jokerWeapon');
-let batmanWeapon = document.querySelector('#batmanWeapon');
-let player1Health = document.querySelector('#player1Health');
-let player2Health = document.querySelector('#player2Health');
-let wins1 = document.querySelector('#wins1');
-let wins2 = document.querySelector('#wins2');
-
-
   startGame();
   function startGame() {
     startWrapp.onclick = function () {
